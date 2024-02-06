84b00f1430ee2ab2a7e7fd371edba5d15a1e2742
Bob Clough <bob.clough@codethink.co.uk>
2024-02-06 16:18:20 +0000

    add a main.py that displays a hardcoded nickname, spins it (badly), then runs nodule identification code

---
+++ bringup-software/main.py
@@ -0,0 +1,26 @@
+# main.py -- put your code here!
+
+import nodulos
+
+import gc9a01py as gc
+import vga2_bold_16x16 as font
+import time
+
+n = nodulos.nodulos()
+
+n.init_display()
+n.tft.fill(gc.WHITE)
+
+s = "@thinkl33t"
+
+w = len(s) * 16
+
+n.tft.text(font, s, int(120-(w/2)), int(120-(16/2)), gc.BLACK, gc.WHITE)
+
+for r in [2, 3, 0, 1, 2]:
+    n.tft.rotation(r)
+    n.tft.fill(gc.WHITE)
+    n.tft.text(font, s, int(120-(w/2)), int(120-(16/2)), gc.BLACK, gc.WHITE)
+    time.sleep(0.5)
+
+n.indicate_nodule_insertion()
