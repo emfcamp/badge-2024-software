7e57f56b3c697ff8084b37597bfae08f18d22e63
Skyler Mansfield <skyler.mansfield.21@gmail.com>
2024-03-27 20:32:24 +0000

    I2C Expansion: Start using the new classes
    tildagonos should use the newly implemented interfaces for I2C mux/GPIO
    expanders.

--- modules/tildagonos.py
+++ modules/tildagonos.py
@@ -1,6 +1,8 @@
 from machine import Pin, I2C, SPI
 import neopixel, time
 import gc9a01py as gc9a01
+from tca9548a import TCA9548A
+from aw9523b import AW9523B, AW9523BPin
 
 BUS_SYSTEM=7
 BUS_TOP=0
@@ -32,12 +34,20 @@ class tildagonos:
         self.pin_reset_i2c=Pin(9,Pin.OUT)
         self.pin_reset_i2c.on()
         self.system_i2c=I2C(0, scl=Pin(46), sda=Pin(45), freq=400000)
-        self.cur_i2c_bus=None
+        self.i2c_expansion=TCA9548A(self.system_i2c, 0x77)
         self.init_gpio()
         self.leds=neopixel.NeoPixel(Pin(21),19)
         self.gpiodata={}
         self.spi=None
         self.tft=None
+        self.pin_system_irq=Pin(10,Pin.IN)
+        self.pin_system_irq.irq(self.system_irq_handler, Pin.IRQ_RISING)
+
+        self.egpio_a = AW9523B(self.i2c_expansion.get_downstream_bus(BUS_SYSTEM), 0x58)
+        self.egpio_b = AW9523B(self.i2c_expansion.get_downstream_bus(BUS_SYSTEM), 0x59)
+        self.egpio_c = AW9523B(self.i2c_expansion.get_downstream_bus(BUS_SYSTEM), 0x5a)
+
+        self.LED_POWER = AW9523BPin(self.egpio_a, (0, 2), mode=Pin.ALT)
         
     def init_display(self):
         self.spi=SPI(1,40000000, sck=Pin(8), mosi=Pin(7))
@@ -45,52 +55,35 @@ class tildagonos:
         self.tft.fill(gc9a01.MAGENTA)
         
     def init_gpio(self):
-        prev_bus=self.cur_i2c_bus
-        self.sel_i2c_bus(BUS_SYSTEM)
-        #egpio reset
-        self.system_i2c.writeto_mem(0x58, 0x7f, bytes([0x00]))
-        self.system_i2c.writeto_mem(0x59, 0x7f, bytes([0x00]))
-        self.system_i2c.writeto_mem(0x5a, 0x7f, bytes([0x00]))
-        #chip A - disable interrupts 
-        self.system_i2c.writeto_mem(0x58, 0x06, bytes([0xff,0xff]))
-        #chip A - set everything to input
-        self.system_i2c.writeto_mem(0x58, 0x04, bytes([0xff,0xff]))
-        #chip A - switch mode to push-pull
-        self.system_i2c.writeto_mem(0x58, 0x11, bytes([0x10]))
-        
-        #chip B - disable interrupts
-        self.system_i2c.writeto_mem(0x59, 0x06, bytes([0xff,0xff]))
-        #chip B - set everything to input
-        self.system_i2c.writeto_mem(0x59, 0x04, bytes([0xff,0xff]))
-        #chip B - switch mode to push-pull
-        self.system_i2c.writeto_mem(0x59, 0x11, bytes([0x10]))
-        #chip C - disable interrupts
-        self.system_i2c.writeto_mem(0x5a, 0x06, bytes([0xff,0xff]))
+        self.i2c_expansion.save_downstream()
+        sys_i2c = self.i2c_expansion.get_downstream_bus(BUS_SYSTEM)
+        self.egpio_a.init()
+        self.egpio_b.init()
+        self.egpio_c.init()
+        #I'd like this to not be direct access but it works as is.
         #chip C - set P0 bits 0,1,3,6,7 and P1 bits 0,1,2,3,4,5,6,7 to input
-        self.system_i2c.writeto_mem(0x5a, 0x04, bytes([0xcb,0xff]))
+        sys_i2c.writeto_mem(0x5a, 0x04, bytes([0xcb,0xff]))
         #chip C - set P0_4(PMID_SW) to 0, set P0_5(USBSEL) to 0, set P0_2(led_power_en) to 0
-        self.system_i2c.writeto_mem(0x5a, 0x02, bytes([0x00]))
-        #chip C - switch mode to push-pull
-        self.system_i2c.writeto_mem(0x5a, 0x11, bytes([0x10]))
-        self.sel_i2c_bus(prev_bus)
+        sys_i2c.writeto_mem(0x5a, 0x02, bytes([0x00]))
+        self.i2c_expansion.restore_downstream()
         
     def set_egpio_pin(self, pin, state):
-        prev_bus=self.cur_i2c_bus
-        self.sel_i2c_bus(BUS_SYSTEM)
-        portstates=list(map(int,self.system_i2c.readfrom_mem(pin[0],0x02,2)))
-        if state:
-            self.system_i2c.writeto_mem(0x5a, 0x02+pin[1], bytes([portstates[pin[1]]|pin[2]]))
-        else:
-            self.system_i2c.writeto_mem(0x5a, 0x02+pin[1], bytes([portstates[pin[1]]&(pin[2]^0xff)]))
-        self.sel_i2c_bus(prev_bus)
+        self.i2c_expansion.save_downstream()
+        egpios = [self.egpio_a, self.egpio_b, self.egpio_c]
+        egpio = egpios[pin[0]-0x59]
+        pinidx = 0
+        v = pin[2]
+        while (v := v//2):
+            pinidx += 1
+        egpio.pin_write((pin[1], pinidx), state)
+        self.i2c_expansion.restore_downstream()
         
     def read_egpios(self):
-        prev_bus=self.cur_i2c_bus
-        self.sel_i2c_bus(BUS_SYSTEM)
-        for i in [0x58, 0x59, 0x5a]:
-            portstates=list(map(int,self.system_i2c.readfrom_mem(i,0x00,2)))
-            self.gpiodata[i]=tuple(portstates)
-        self.sel_i2c_bus(prev_bus)
+        self.i2c_expansion.save_downstream()
+        for eg in [self.egpio_a, self.egpio_b, self.egpio_c]:
+            portstates=list(map(int,eg.bus.readfrom_mem(eg.addr,0x00,2)))
+            self.gpiodata[eg.addr]=tuple(portstates)
+        self.i2c_expansion.restore_downstream()
         
     def check_egpio_state(self, pin, readgpios=True):
         if not pin[0] in self.gpiodata or readgpios:
@@ -98,7 +91,9 @@ class tildagonos:
         return bool(pin[2]&self.gpiodata[pin[0]][pin[1]])
     
     def set_led_power(self, state):
-        self.set_egpio_pin(EPIN_LED_POWER, state)
+        self.i2c_expansion.save_downstream()
+        self.LED_POWER.value(state)
+        self.i2c_expansion.restore_downstream()
         
     def indicate_hexpansion_insertion(self):
         self.set_led_power(True)
@@ -119,6 +114,10 @@ class tildagonos:
             time.sleep(0.1)
             
     def sel_i2c_bus(self, bus=None):
+        """
+        TO BE DEPRACATED
+        use the TCA9548 bus wrappper instead!
+        """
         if bus is not None and bus >= 0 and bus <=7:
             self.system_i2c.writeto(0x77,bytes([(0x1<<bus)]))
             self.cur_i2c_bus=bus
