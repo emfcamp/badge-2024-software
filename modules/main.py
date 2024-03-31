9f438bbdf5255b5ff34073c9fb9c6c5f1856cc53
Matthew Wilkes <git@matthewwilkes.name>
2024-03-31 18:34:27 +0100

    Fiddle with the opening animation a bit

--- modules/main.py
+++ modules/main.py
@@ -10,14 +10,18 @@ n = tildagonos()
 
 display.gfx_init()
 
-s = "@thinkl33t"
+s = "EMF Camp"
 
-for i in range(7):
-    ctx = display.get_ctx().save().rgb(255,255,255).rectangle(-120, -120, 240, 240).fill()
+spins = 3
+its = spins*6+1
+for i in range(its):
+    ctx = display.get_ctx().save().gray(1-(i/its)).rectangle(-120, -120, 240, 240).fill()
+    s_width = ctx.text_width(s)
+    s_height = ctx.font_size
     ctx = ctx.linear_gradient(-50,-50, 50, 50).add_stop(0.0, (1.0, 0.0, 0.0), 1.0).add_stop(0.5, (0.0, 1.0, 0.0), 1.0)
-    ctx = ctx.add_stop(1.0, (0.0, 0.0, 1.0), 1.0).move_to(-70, 0).rotate(i*math.pi/3).text("EMF Camp").restore()
+    ctx = ctx.add_stop(1.0, (0.0, 0.0, 1.0), 1.0).move_to(0-s_width/2, s_height/4).rotate(i*math.pi/3).text(s).restore()
     display.end_frame(ctx)
-    time.sleep(0.25)
+    time.sleep(0.05)
 
 
 def yay():
