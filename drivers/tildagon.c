13d0f9b0d0bc25efccbae6da4ea0f175bff9dbde
Skyler Mansfield <skyler.mansfield.21@gmail.com>
2024-04-01 20:21:45 +0100

    i2c mux: add i2c port initialisation
    initialisation of the I2C MUX now initialises the I2C driver as well.

--- drivers/tildagon.c
+++ drivers/tildagon.c
@@ -10,6 +10,19 @@ const tca9548a_i2c_mux_t *tildagon_get_i2c_mux() {
     }
     xSemaphoreGive(&tildagon_i2c_mux.mtx);
     tildagon_i2c_mux.addr = 0x77;
+    
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = TILDAGON_HOST_I2C_SDA,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = TILDAGON_HOST_I2C_SCL,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = TILDAGON_HOST_I2C_FREQ,
+    };
+    i2c_param_config(TILDAGON_HOST_I2C_PORT, &conf);
+    int timeout = I2C_SCLK_FREQ / 1000000 * timeout_us;
+    i2c_set_timeout(TILDAGON_HOST_I2C_PORT, (timeout > I2C_LL_MAX_TIMEOUT) ? I2C_LL_MAX_TIMEOUT : timeout);
+    i2c_driver_install(TILDAGON_HOST_I2C_PORT, I2C_MODE_MASTER, 0, 0, 0);
   }
   return &tildagon_i2c_mux;
 }
--- drivers/tildagon.h
+++ drivers/tildagon.h
@@ -3,6 +3,11 @@
 
 #include "tca9548a.h"
 
+#define TILDAGON_HOST_I2C_SDA (8)
+#define TILDAGON_HOST_I2C_SCL (9)
+#define TILDAGON_HOST_I2C_FREQ (400000)
+#define TILDAGON_HOST_I2C_PORT (0)
+
 const tca9548a_i2c_mux_t *tildagon_get_i2c_mux();
 
 
