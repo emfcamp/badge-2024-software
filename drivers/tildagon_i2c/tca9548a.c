9224034b61df313a82366faecc83dcf572a82e73
b0xcat <me@b0xcat.nl>
2024-04-08 11:23:40 +0200

    tca9548: Set downstream i2c mux bus in a separate i2c transaction, as adding additional commands to an i2c cmd link after stopping is not supported

--- drivers/tildagon_i2c/tca9548a.c
+++ drivers/tildagon_i2c/tca9548a.c
@@ -4,16 +4,22 @@
 #include "tca9548a.h"
 
 
-void tca9548a_cmd_set_downstream(const tca9548a_i2c_mux_t *self, tca9548a_i2c_port_t port, i2c_cmd_handle_t cmd) {
+esp_err_t tca9548a_cmd_set_downstream(const tca9548a_i2c_mux_t *self, tca9548a_i2c_port_t port) {
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, self->addr << 1, true);
     i2c_master_write_byte(cmd, (1<<port), true);
-    i2c_master_stop(cmd);   
+    i2c_master_stop(cmd);
+
+    esp_err_t ret = i2c_master_cmd_begin(self->port, cmd, 100 * (3 + 2) / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
 }
 
 i2c_cmd_handle_t tca9548a_cmd_link_create(const tca9548a_i2c_mux_t *self, tca9548a_i2c_port_t port) {
+    tca9548a_cmd_set_downstream(self, port);
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    tca9548a_cmd_set_downstream(self, port, cmd);
     return cmd;
 }
 
--- drivers/tildagon_i2c/tca9548a.h
+++ drivers/tildagon_i2c/tca9548a.h
@@ -17,7 +17,7 @@ typedef unsigned char tca9548a_i2c_port_t;
  * tca9548a_cmd_link_create
  * 
  * Similar to i2c_cmd_link_create - creates an i2c_cmd_handle_t
- * and additionally adds a command to set the downstream port.
+ * and additionally performs a transaction to set the downstream port.
  * This function is thread-safe and can be called from any thread.
  * Be aware that a longer timeout may be needed if the bus is busy!
  *
@@ -38,4 +38,14 @@ i2c_cmd_handle_t tca9548a_cmd_link_create(const tca9548a_i2c_mux_t *self, tca954
 */
 esp_err_t tca9548a_master_cmd_begin(const tca9548a_i2c_mux_t *self, i2c_cmd_handle_t cmd, TickType_t ticks_to_wait);
 
+/**
+ * tca9548a_cmd_set_downstream
+ * 
+ * Enables the specified downstream port on the I2C provided i2c mux.
+ * Only one port is active at a time.
+ * 
+ * @param self - mux object to set the downstream port for
+ * @param port - the port to switch to
+*/
+esp_err_t tca9548a_cmd_set_downstream(const tca9548a_i2c_mux_t *self, tca9548a_i2c_port_t port);
 #endif // _TCA9548A_H
