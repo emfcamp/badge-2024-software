f803bd496167a3f7517193ad6eac84b7c8b8b877
b0xcat <me@b0xcat.nl>
2024-04-19 22:18:34 +0200

    Make main.py start the scheduler and some demo apps

--- modules/main.py
+++ modules/main.py
@@ -1,41 +1,20 @@
 # main.py -- put your code here!
 
 from tildagonos import tildagonos
+from scheduler import Scheduler
+from apps.indicate_hexpansion import HexpansionInsertionApp
+from apps.tick_app import TickApp
+from apps.intro_app import IntroApp
 import display
-import time
-import random
-import math
+
+print("Oh hi")
 
 n = tildagonos()
-
 display.gfx_init()
 
-s = "EMF Camp"
+sc = Scheduler()
+sc.start_app(HexpansionInsertionApp(tildagonos=n))
+sc.start_app(TickApp())
+sc.start_app(IntroApp(), foreground=True)
 
-spins = 3
-its = spins*6+1
-for i in range(its):
-    ctx = display.get_ctx().save().gray(1-(i/its)).rectangle(-120, -120, 240, 240).fill()
-    s_width = ctx.text_width(s)
-    s_height = ctx.font_size
-    ctx = ctx.linear_gradient(-50,-50, 50, 50).add_stop(0.0, (1.0, 0.0, 0.0), 1.0).add_stop(0.5, (0.0, 1.0, 0.0), 1.0)
-    ctx = ctx.add_stop(1.0, (0.0, 0.0, 1.0), 1.0).move_to(0-s_width/2, s_height/4).rotate(i*math.pi/3).text(s).restore()
-    display.end_frame(ctx)
-    time.sleep(0.05)
-
-
-def bestagons(n):
-    while n:
-        red = random.random()
-        blue = random.random()
-        green = random.random()
-        size = random.randint(10, 100)
-        ctx = display.get_ctx().save().rgba(red,green,blue, 0.2)
-        display.hexagon(ctx, random.randint(-120, 120), random.randint(-120, 120), size)
-        ctx = ctx.restore()
-        display.end_frame(ctx)
-        n -= 1
-
-
-bestagons(10)
-n.indicate_hexpansion_insertion()
+sc.run_forever()
