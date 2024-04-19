1fb37162f04feba7931cbc2dc06500a37c102762
b0xcat <me@b0xcat.nl>
2024-04-19 22:17:46 +0200

    Add initial WIP attempt for app scheduler

---
+++ modules/scheduler.py
@@ -0,0 +1,78 @@
+import asyncio
+import time
+import display
+
+from perf_timer import PerfTimer
+
+TARGET_FRAMETIME_US = 16_000 # Not really reachable I guess lol
+
+class AppInfo:
+    def __init__(self, app):
+        self.app = app
+        self.running = False
+        self.last_updated = time.ticks_us()
+        self.background_tasks = []
+
+
+class Scheduler:
+    def __init__(self):
+        self.current_app = None
+        self.apps = []
+        self.background_tasks = []
+
+        self.last_render_time = time.ticks_us()
+        self.last_update_times = []
+
+    def start_app(self, app, foreground=False):
+        self.apps.append(app)
+        self.last_update_times.append(time.ticks_us())
+        if foreground:
+            self.current_app = app
+
+    async def _start_background_tasks(self, app):
+        # TODO: check if this is async if possible? And more sanity checks. Maybe this is not the way to do it?
+        try:
+            self.background_tasks.append(asyncio.create_task(app.background_update()))
+        except AttributeError:
+            pass
+
+    async def _update_task(self):
+        while True:
+            with PerfTimer("updates"):
+                for idx, app in enumerate(self.apps):
+                    cur_time = time.ticks_us()
+                    delta_time = time.ticks_diff(cur_time, self.last_update_times[idx])
+                    app.update(delta_time)
+                    self.last_update_times[idx] = cur_time
+            await asyncio.sleep(0)
+
+    async def _render_task(self):
+        while True:
+            with PerfTimer("render"):
+                cur_time = time.ticks_us()
+                time_since_last_render = time.ticks_diff(cur_time, self.last_render_time)
+
+                if time_since_last_render >= TARGET_FRAMETIME_US:
+                    self.last_render_time = cur_time
+                    ctx = display.get_ctx()
+                    ctx.save()
+                    if self.current_app:
+                        self.current_app.draw(ctx)
+                    ctx.restore()
+                    display.end_frame(ctx)
+
+                time_after_render = time.ticks_us()
+                render_took = time.ticks_diff(time_after_render, cur_time)
+
+                time_to_next_render = max(0, (TARGET_FRAMETIME_US - render_took) / 1000_000)
+            await asyncio.sleep(time_to_next_render)
+
+    async def _main(self):
+        for app in self.apps:
+            await self._start_background_tasks(app)
+        update_task = asyncio.create_task(self._update_task())
+        render_task = asyncio.create_task(self._render_task())
+        await asyncio.gather(update_task, render_task)
+
+    def run_forever(self):
+        asyncio.run(self._main())
