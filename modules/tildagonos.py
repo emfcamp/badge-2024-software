42c3d1a14fa673ea6b6753c2eb6e273018e6a4a5
b0xcat <me@b0xcat.nl>
2024-04-19 22:15:55 +0200

    Make indicate hexpansion insertion async so it can be ran in the background and add perf timer

--- modules/tildagonos.py
+++ modules/tildagonos.py
@@ -1,6 +1,8 @@
 from machine import Pin, I2C, SPI
+from perf_timer import PerfTimer
 import neopixel, time
 import gc9a01py as gc9a01
+import asyncio
 
 BUS_SYSTEM = 7
 BUS_TOP = 0
@@ -91,20 +93,21 @@ class tildagonos:
     def set_led_power(self, state):
         self.set_egpio_pin(EPIN_LED_POWER, state)
 
-    def indicate_hexpansion_insertion(self):
+    async def indicate_hexpansion_insertion(self):
         self.set_led_power(True)
         while True:
-            self.read_egpios()
-            self.leds.fill((0, 0, 0))
-            for i, n in enumerate([EPIN_ND_A, EPIN_ND_B, EPIN_ND_C, EPIN_ND_D, EPIN_ND_E, EPIN_ND_F]):
-                if not self.check_egpio_state(n):
-                    self.leds[13 + i] = led_colours[i]
-            for i, n in enumerate([EPIN_BTN_1, EPIN_BTN_2, EPIN_BTN_3, EPIN_BTN_4, EPIN_BTN_5, EPIN_BTN_6]):
-                if not self.check_egpio_state(n):
-                    if i:
-                        self.leds[i * 2] = led_colours[i]
-                    else:
-                        self.leds[12] = led_colours[i]
-                    self.leds[1 + i * 2] = led_colours[i]
-            self.leds.write()
-            time.sleep(0.1)
+            with PerfTimer("indicate hexpansion insertion"):
+                self.read_egpios()
+                self.leds.fill((0, 0, 0))
+                for i, n in enumerate([EPIN_ND_A, EPIN_ND_B, EPIN_ND_C, EPIN_ND_D, EPIN_ND_E, EPIN_ND_F]):
+                    if not self.check_egpio_state(n):
+                        self.leds[13 + i] = led_colours[i]
+                for i, n in enumerate([EPIN_BTN_1, EPIN_BTN_2, EPIN_BTN_3, EPIN_BTN_4, EPIN_BTN_5, EPIN_BTN_6]):
+                    if not self.check_egpio_state(n):
+                        if i:
+                            self.leds[i * 2] = led_colours[i]
+                        else:
+                            self.leds[12] = led_colours[i]
+                        self.leds[1 + i * 2] = led_colours[i]
+                self.leds.write()
+            await asyncio.sleep(0.1)
