7a58086db1baabdbf3d6cf273e2896f14f6c5cdc
b0xcat <me@b0xcat.nl>
2024-04-20 15:35:30 +0200

    Easier testing (repl - from scratchpad import *)
    For some reason mpremote mount doesn't replace main properly

--- modules/main.py
+++ modules/main.py
@@ -1,20 +1,21 @@
 # main.py -- put your code here!
-
-from tildagonos import tildagonos
-from scheduler import Scheduler
-from apps.indicate_hexpansion import HexpansionInsertionApp
-from apps.tick_app import TickApp
-from apps.intro_app import IntroApp
-import display
+#
+# from tildagonos import tildagonos
+# from scheduler import Scheduler
+# from apps.indicate_hexpansion import HexpansionInsertionApp
+# from apps.tick_app import TickApp
+# from apps.intro_app import IntroApp
+#
+# import display
 
 print("Oh hi")
 
-n = tildagonos()
-display.gfx_init()
-
-sc = Scheduler()
-sc.start_app(HexpansionInsertionApp(tildagonos=n))
-sc.start_app(TickApp())
-sc.start_app(IntroApp(), foreground=True)
-
-sc.run_forever()
+# n = tildagonos()
+# display.gfx_init()
+#
+# sc = Scheduler()
+# sc.start_app(HexpansionInsertionApp(tildagonos=n))
+# sc.start_app(TickApp())
+# sc.start_app(IntroApp(text="I'm gay"), foreground=True)
+#
+# sc.run_forever()
---
+++ modules/scratchpad.py
@@ -0,0 +1,24 @@
+
+from tildagonos import tildagonos
+from scheduler import Scheduler
+from apps.indicate_hexpansion import HexpansionInsertionApp
+from apps.tick_app import TickApp
+from apps.intro_app import IntroApp
+from apps.pingpong_app import PingApp, PongApp
+
+import display
+
+print("Oh hi")
+
+n = tildagonos()
+display.gfx_init()
+
+sc = Scheduler()
+
+# sc.start_app(HexpansionInsertionApp(tildagonos=n))
+# sc.start_app(TickApp())
+sc.start_app(IntroApp(text="I'm gay"), foreground=True)
+
+sc.start_app(PingApp())
+sc.start_app(PongApp())
+# sc.run_forever()
