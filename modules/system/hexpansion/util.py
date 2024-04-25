45add8386ac0d5ce01caae800c248bc50680b33e
Matthew Wilkes <git@matthewwilkes.name>
2024-04-25 19:54:58 +0100

    Use old-style optional type, to allow running on CPython<3.10 for the simulator

--- modules/system/hexpansion/util.py
+++ modules/system/hexpansion/util.py
@@ -5,9 +5,10 @@ from eeprom_partition import EEPROMPartition
 from system.hexpansion.header import HexpansionHeader
 
 import vfs
+import typing
 
 
-def read_hexpansion_header(i2c, eeprom_addr=0x50) -> HexpansionHeader | None:
+def read_hexpansion_header(i2c, eeprom_addr=0x50) -> typing.Optional[HexpansionHeader]:
     devices = i2c.scan()
     if eeprom_addr not in devices:
         print(f"No device found at {hex(eeprom_addr)}")
