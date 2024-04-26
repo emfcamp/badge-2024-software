b302f9a685b8c99a6545c7607ab71f667db558c5
b0xcat <me@b0xcat.nl>
2024-04-26 23:09:42 +0200

    Get rid of more hardcoded i2c addresses

--- modules/system/hexpansion/util.py
+++ modules/system/hexpansion/util.py
@@ -7,6 +7,15 @@ from system.hexpansion.header import HexpansionHeader
 import vfs
 
 
+def detect_eeprom_addr(i2c):
+    devices = i2c.scan()
+    if 0x57 in devices and 0x50 not in devices:
+        return 0x57
+    if 0x50 in devices:
+        return 0x50
+    return None
+
+
 def read_hexpansion_header(i2c, eeprom_addr=0x50) -> HexpansionHeader | None:
     devices = i2c.scan()
     if eeprom_addr not in devices:
@@ -25,10 +34,11 @@ def read_hexpansion_header(i2c, eeprom_addr=0x50) -> HexpansionHeader | None:
     return header
 
 
-def get_hexpansion_block_devices(i2c, header):
+def get_hexpansion_block_devices(i2c, header, addr=0x50):
     eep = EEPROM(i2c=i2c,
                  chip_size=header.eeprom_total_size,
-                 page_size=header.eeprom_page_size)
+                 page_size=header.eeprom_page_size,
+                 addr=addr)
     partition = EEPROMPartition(eep=eep,
                                 offset=header.fs_offset,
                                 length=header.eeprom_total_size - header.fs_offset)
--- modules/write_header.py
+++ modules/write_header.py
@@ -2,30 +2,30 @@ from system.hexpansion.header import HexpansionHeader
 from machine import I2C
 
 
-def write_header(port):
+def write_header(port, addr=0x50):
     i2c = I2C(port)
 
     h = HexpansionHeader(
         manifest_version="2024",
-        fs_offset=64,
-        eeprom_page_size=64,
+        fs_offset=32,
+        eeprom_page_size=32,
         eeprom_total_size=1024 * 64,
         vid=0xCA75,
         pid=0x1337,
         unique_id=0x0,
-        friendly_name="booper"
+        friendly_name="internal"
     )
 
     # Write header to 0x00 of the eeprom
-    i2c.writeto(0x50, bytes([0, 0]) + h.to_bytes())
+    i2c.writeto(addr, bytes([0, 0]) + h.to_bytes())
 
-def read_header(port):
+def read_header(port, addr=0x50):
     i2c = I2C(port)
 
     # Set internal address to 0x00
-    i2c.writeto(0x50, bytes([0, 0]))
+    i2c.writeto(addr, bytes([0, 0]))
 
-    header_bytes = i2c.readfrom(0x50, 32)
+    header_bytes = i2c.readfrom(addr, 32)
     header = HexpansionHeader.from_bytes(header_bytes)
 
     return header
