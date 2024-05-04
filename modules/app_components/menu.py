064d6d00b08f5a73f0ba2124e2c2e9f66329487a
Hugh Rawlinson <hughr2005@gmail.com>
2024-05-04 19:02:52 +0200

    Add menu widget

---
+++ modules/app_components/menu.py
@@ -0,0 +1,77 @@
+from typing import Callable, Literal, Union
+
+from app import App
+from events.input import ButtonDownEvent
+from system.eventbus import eventbus
+
+
+class Menu:
+    def __init__(
+        self,
+        app: App,
+        menu_items: list[str] = [],
+        position=0,
+        select_handler: Union[Callable, None] = None,
+        back_handler: Union[Callable, None] = None,
+        animation_time_ms=300,
+    ):
+        self.menu_items = menu_items
+        self.position = position
+        self.select_handler = select_handler
+        self.back_handler = back_handler
+        self.animation_time_ms = animation_time_ms
+        self.is_animating: Literal["up", "down", "none"] = "none"
+        self.app = app
+
+        eventbus.on(ButtonDownEvent, self._handle_buttondown, app)
+
+    def _cleanup(self):
+        eventbus.remove(ButtonDownEvent, self._handle_buttondown, self.app)
+
+    def _handle_buttondown(self, event: ButtonDownEvent):
+        match event.button:
+            case 0:
+                self.up_handler()
+            case 3:
+                self.down_handler()
+            case 5:
+                if self.back_handler is not None:
+                    self.back_handler()
+            case 1:
+                if self.select_handler is not None:
+                    self.select_handler(
+                        self.menu_items[self.position % len(self.menu_items)]
+                    )
+
+    def up_handler(self):
+        self.position = (self.position - 1) % len(self.menu_items)
+
+    def down_handler(self):
+        self.position = (self.position + 1) % len(self.menu_items)
+
+    def draw(self, ctx):
+        # Current menu item
+        ctx.font_size = 40
+        ctx.text_align = ctx.CENTER
+        ctx.text_baseline = ctx.MIDDLE
+        text_height = ctx.font_size
+        focused_item_margin = 20
+        line_height = 30
+
+        ctx.rgb(1, 1, 1)
+        ctx.move_to(0, 0).text(self.menu_items[self.position % len(self.menu_items)])
+
+        # Previous menu items
+        ctx.font_size = 20
+        for i in range(1, 4):
+            if (self.position - i) >= 0:
+                ctx.move_to(0, -focused_item_margin + -i * line_height).text(
+                    self.menu_items[self.position - i]
+                )
+
+        # Next menu items
+        for i in range(1, 4):
+            if (self.position + i) < len(self.menu_items):
+                ctx.move_to(0, focused_item_margin + i * line_height).text(
+                    self.menu_items[self.position + i]
+                )
---
+++ modules/apps/menu_demo.py
@@ -0,0 +1,79 @@
+from typing import Literal
+
+from app import App
+from app_components.menu import Menu
+
+main_menu_items = ["numbers", "letters", "words"]
+
+numbers_menu_items = ["one", "two", "three", "four", "five"]
+
+letters_menu_items = ["a", "b", "c", "d", "e"]
+
+words_menu_items = ["emfcamp", "bodgeham-on-wye", "hackers", "hexpansions", "tildagon"]
+
+
+class MenuDemo(App):
+    def __init__(self):
+        self.current_menu = "main"
+        self.menu = Menu(
+            self,
+            main_menu_items,
+            select_handler=self.select_handler,
+            back_handler=self.back_handler,
+        )
+
+    def select_handler(self, item):
+        print(item)
+        if item in ["numbers", "letters", "words", "main"]:
+            self.set_menu(item)
+        else:
+            # flash or spin the word or something
+            pass
+
+    def set_menu(self, menu_name: Literal["main", "numbers", "letters", "words"]):
+        self.menu._cleanup()
+        self.current_menu = menu_name
+        match menu_name:
+            case "main":
+                self.menu = Menu(
+                    self,
+                    main_menu_items,
+                    select_handler=self.select_handler,
+                    back_handler=self.back_handler,
+                )
+            case "numbers":
+                self.menu = Menu(
+                    self,
+                    numbers_menu_items,
+                    select_handler=self.select_handler,
+                    back_handler=self.back_handler,
+                )
+            case "letters":
+                self.menu = Menu(
+                    self,
+                    letters_menu_items,
+                    select_handler=self.select_handler,
+                    back_handler=self.back_handler,
+                )
+            case "words":
+                self.menu = Menu(
+                    self,
+                    words_menu_items,
+                    select_handler=self.select_handler,
+                    back_handler=self.back_handler,
+                )
+
+    def back_handler(self):
+        if self.current_menu == "main":
+            return
+        self.set_menu("main")
+
+    def draw_background(self, ctx):
+        ctx.gray(0).rectangle(-120, -120, 240, 240).fill()
+
+    def draw(self, ctx):
+        self.draw_background(ctx)
+        self.menu.draw(ctx)
+
+    def update(self, delta):
+        pass
