e6d9f93ef771b0cf57757927c8924384e04981a7
Matthew Wilkes <git@matthewwilkes.name>
2024-05-08 20:08:40 +0100

    Example app in the simulator

---
+++ sim/apps/example/app.py
@@ -0,0 +1,19 @@
+import asyncio
+import app
+
+from events.input import Buttons, BUTTON_TYPES
+
+
+class ExampleApp(app.App):
+    def __init__(self):
+        self.button_states = Buttons(self)
+
+    def update(self, delta):
+        if self.button_states.get(BUTTON_TYPES["CANCEL"]):
+            self.minimise()
+
+    def draw(self, ctx):
+        ctx.save()
+        ctx.rgb(0.2,0,0).rectangle(-120,-120,240,240).fill()
+        ctx.rgb(1,0,0).move_to(-80,0).text("Hello world")
+        ctx.restore()
