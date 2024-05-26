934d7e055acea2557a18b8924f61e909efc5d41c
Hugh Rawlinson <hughr2005@gmail.com>
2024-05-26 19:21:24 +0200

    WIP

--- modules/firmware_apps/app_store.py
+++ modules/firmware_apps/app_store.py
@@ -4,11 +4,12 @@ import app
 import display
 import wifi
 from app_components import Menu, clear_background
+from events.input import BUTTON_TYPES, Buttons
 from urequests import get
 
 APP_STORE_LISTING_URL = "https://apps.badge.emfcamp.org/demo_api/apps.json"
 
-BOOP_INSTALL = "BoopInstall"
+CODE_INSTALL = "CodeInstall"
 AVAILABLE = "Available"
 INSTALLED = "Installed"
 UPDATE = "Update"
@@ -26,7 +27,12 @@ class AppStoreApp(app.App):
         super().__init__()
         self.menu = Menu(
             self,
-            menu_items=[BOOP_INSTALL, AVAILABLE, UPDATE, INSTALLED],
+            menu_items=[
+                CODE_INSTALL,
+                # AVAILABLE,
+                # UPDATE,
+                # INSTALLED
+            ],
             select_handler=self.on_select,
             back_handler=self.on_cancel,
         )
@@ -34,6 +40,7 @@ class AppStoreApp(app.App):
         self.installed_menu = None
         self.update_menu = None
         self.app_store_index = []
+        self.button_states = Buttons(self)
         self.get_index()
 
     def check_wifi(self):
@@ -64,20 +71,28 @@ class AppStoreApp(app.App):
         self.available_menu = Menu(
             self,
             menu_items=[app["manifest"]["app"]["name"] for app in self.app_store_index],
-            select_handler=lambda _, i: install_app(self.app_store_index[i]),
+            select_handler=lambda _, i: self.install_app(self.app_store_index[i]),
         )
         self.update_state("main_menu")
+
+    def install_app(self, app):
+        print(f"Installing {app['name']}")
 
     def update_state(self, state):
         print(f"State Transition: '{self.state}' -> '{state}'")
         self.state = state
 
-    def on_select(self, value):
-        if value == BOOP_INSTALL:
-            self.boopinstall = BoopInstall(
-                install_handler=lambda id: print(f"Installing {id}")
+    def handle_code_input(self, app):
+        print(f"Installing {app}")
+        self.update_state("main_menu")
+
+    def on_select(self, value, idx):
+        if value == CODE_INSTALL:
+            self.codeinstall = CodeInstall(
+                install_handler=lambda id: self.handle_code_input(id),
+                buttons=self.button_states,
             )
-            self.update_state("boop_install")
+            self.update_state("code_install_input")
         elif value == AVAILABLE:
             self.update_state("available_menu")
         elif value == INSTALLED:
@@ -102,6 +117,8 @@ class AppStoreApp(app.App):
             self.installed_menu.update(delta)
         if self.update_menu:
             self.update_menu.update(delta)
+        if self.codeinstall:
+            self.codeinstall.update()
         return super().update(delta)
 
     def draw(self, ctx):
@@ -123,6 +140,8 @@ class AppStoreApp(app.App):
             self.error_screen(ctx, "Checking\nWi-Fi connection")
         elif self.state == "refreshing_index":
             self.error_screen(ctx, "Refreshing app store index")
+        elif self.state == "code_install_input" and self.codeinstall:
+            self.codeinstall.draw(ctx)
         else:
             self.error_screen(ctx, "Unknown error")
         ctx.restore()
@@ -130,15 +149,35 @@ class AppStoreApp(app.App):
         self.draw_overlays(ctx)
 
 
-class BoopInstall:
-    def __init__(self, install_handler: Callable[[str], Any]):
+class CodeInstall:
+    def __init__(self, install_handler: Callable[[str], Any], buttons: Buttons):
         self.install_handler = install_handler
         self.state = "input"
         self.id: str = ""
+        self.buttons_state = buttons
 
     def draw(self, ctx):
         ctx.save()
         ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
         ctx.rgb(1, 0, 0)
         display.hexagon(ctx, 0, 0, 80)
+        ctx.text_align = ctx.CENTER
+        ctx.text_baseline = ctx.MIDDLE
+        ctx.move_to(0, 0).text(id)
         ctx.restore()
+
+    def update(self):
+        if len(self.id) == 8:
+            self.install_handler(self.id)
+        if self.buttons_state.get(BUTTON_TYPES["UP"]):
+            self.id += "0"
+        elif self.buttons_state.get(BUTTON_TYPES["RIGHT"]):
+            self.id += "1"
+        elif self.buttons_state.get(BUTTON_TYPES["CONFIRM"]):
+            self.id += "2"
+        elif self.buttons_state.get(BUTTON_TYPES["DOWN"]):
+            self.id += "3"
+        elif self.buttons_state.get(BUTTON_TYPES["LEFT"]):
+            self.id += "4"
+        elif self.buttons_state.get(BUTTON_TYPES["CANCEl"]):
+            self.id += "5"
