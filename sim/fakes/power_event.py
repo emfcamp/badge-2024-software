484c6a7f9e534922ee61247b17b99339af242f98
Matthew Wilkes <git@matthewwilkes.name>
2024-05-29 23:06:14 +0100

    Mock power event handlers

---
+++ sim/fakes/power_event.py
@@ -0,0 +1,23 @@
+def set_charge_cb(handler):
+    pass
+
+def set_device_attach_cb(handler):
+    pass
+
+def set_device_detach_cb(handler):
+    pass
+
+def set_fault_cb(handler):
+    pass
+
+def set_host_attach_cb(handler):
+    pass
+
+def set_host_detach_cb(handler):
+    pass
+
+def set_lanyard_attach_cb(handler):
+    pass
+
+def set_lanyard_detach_cb(handler):
+    pass
