e8d716ad249e77833d2ef2865fb29c304a85203c
Matthew Wilkes <git@matthewwilkes.name>
2024-05-31 12:14:46 +0100

    Fixes for on-app launching
    This may not be working on the simulator, can someone check before merging?

--- modules/system/launcher/app.py
+++ modules/system/launcher/app.py
@@ -41,12 +41,9 @@ def recursive_delete(path):
     os.rmdir(path)
 
 
-def load_info(folder, name, sim):
+def load_info(folder, name):
     try:
-        if sim:
-            info_file = "{}/{}/metadata.json".format(folder, name)
-        else:
-            info_file = "{}/{}/__internal__metadata.json".format(folder, name)
+        info_file = "{}/{}/metadata.json".format(folder, name)
         with open(info_file) as f:
             information = f.read()
         return json.loads(information)
@@ -61,21 +58,20 @@ def list_user_apps():
             contents = os.listdir(APP_DIR)
         except OSError:
             # No apps dir full stop
+            try:
+                os.mkdir(APP_DIR)
+            except OSError:
+                pass
             return []
 
         for name in contents:
-            sim = path_isfile(f"{APP_DIR}/{name}/__init__.py")
-            store = path_isfile(f"{APP_DIR}/{name}/app.py")
-            if not sim and not store:
-                continue
-
             app = {
-                "path": name,
-                "callable": "main",
+                "path": f"apps.{name}.app",
+                "callable": "__app_export__",
                 "name": name,
                 "hidden": False,
             }
-            metadata = load_info(APP_DIR, name, sim)
+            metadata = load_info(APP_DIR, name)
             app.update(metadata)
             if not app["hidden"]:
                 apps.append(app)
