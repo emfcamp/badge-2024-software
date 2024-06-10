0554a76ba24eede89f2505c0a2f656018b43ac93
Chris <ChrisDick@users.noreply.github.com>
2024-06-10 22:46:48 +0100

    only create pin objects when needed
    reduce ram usage by only creating the pin objects when the hexpansion port is created.

--- modules/hexpansion.py
+++ modules/hexpansion.py
@@ -2,14 +2,93 @@ from tildagon import Pin as ls_pin
 from machine import Pin as hs_pin
 
 
-class port:
+class hexpansion:
     OUT = ls_pin.OUT
     IN = ls_pin.IN
     PWM = ls_pin.PWM
 
-    def __init__(self, LS_A, LS_B, LS_C, LS_D, LS_E, HS_A, HS_B, HS_C, HS_D):
-        self.ls_info = {"A": LS_A, "B": LS_B, "C": LS_C, "D": LS_D, "E": LS_E}
-        self.hs_info = {"A": HS_A, "B": HS_B, "C": HS_C, "D": HS_D}
+    def __init__(self, port):
+        pins = [
+            {
+                "LS_A": (2, 3),
+                "LS_B": (2, 8),
+                "LS_C": (2, 9),
+                "LS_D": (2, 10),
+                "LS_E": (2, 11),
+                "HS_A": 39,
+                "HS_B": 40,
+                "HS_C": 41,
+                "HS_D": 42,
+            },
+            {
+                "LS_A": (2, 0),
+                "LS_B": (2, 1),
+                "LS_C": (1, 13),
+                "LS_D": (1, 14),
+                "LS_E": (1, 15),
+                "HS_A": 35,
+                "HS_B": 36,
+                "HS_C": 37,
+                "HS_D": 38,
+            },
+            {
+                "LS_A": (1, 4),
+                "LS_B": (1, 5),
+                "LS_C": (1, 6),
+                "LS_D": (1, 7),
+                "LS_E": (1, 12),
+                "HS_A": 34,
+                "HS_B": 33,
+                "HS_C": 47,
+                "HS_D": 48,
+            },
+            {
+                "LS_A": (0, 8),
+                "LS_B": (0, 9),
+                "LS_C": (0, 10),
+                "LS_D": (0, 11),
+                "LS_E": (0, 0),
+                "HS_A": 11,
+                "HS_B": 14,
+                "HS_C": 13,
+                "HS_D": 12,
+            },
+            {
+                "LS_A": (0, 2),
+                "LS_B": (0, 3),
+                "LS_C": (0, 4),
+                "LS_D": (0, 5),
+                "LS_E": (0, 14),
+                "HS_A": 18,
+                "HS_B": 16,
+                "HS_C": 15,
+                "HS_D": 17,
+            },
+            {
+                "LS_A": (0, 7),
+                "LS_B": (0, 12),
+                "LS_C": (0, 13),
+                "LS_D": (0, 14),
+                "LS_E": (0, 15),
+                "HS_A": 3,
+                "HS_B": 4,
+                "HS_C": 5,
+                "HS_D": 6,
+            },
+        ]
+        self.ls_info = {
+            "A": pins[port]["LS_A"],
+            "B": pins[port]["LS_B"],
+            "C": pins[port]["LS_C"],
+            "D": pins[port]["LS_D"],
+            "E": pins[port]["LS_E"],
+        }
+        self.hs_info = {
+            "A": pins[port]["HS_A"],
+            "B": pins[port]["HS_B"],
+            "C": pins[port]["HS_C"],
+            "D": pins[port]["HS_D"],
+        }
         self.ls = {"A": None, "B": None, "C": None, "D": None, "E": None}
         self.hs = {"A": None, "B": None, "C": None, "D": None}
         for key in self.ls_info:
@@ -18,18 +97,9 @@ class port:
             self.hs[key] = hs_pin(self.hs_info[key], hs_pin.IN)
 
 
-hexpansion = [
-    port((2, 3), (2, 8), (2, 9), (2, 10), (2, 11), 39, 40, 41, 42),
-    port((2, 0), (2, 1), (1, 13), (1, 14), (1, 15), 35, 36, 37, 38),
-    port((1, 4), (1, 5), (1, 6), (1, 7), (1, 12), 34, 33, 47, 48),
-    port((0, 8), (0, 9), (0, 10), (0, 11), (0, 0), 11, 14, 13, 12),
-    port((0, 2), (0, 3), (0, 4), (0, 5), (0, 14), 18, 16, 15, 17),
-    port((0, 7), (0, 12), (0, 13), (0, 14), (0, 15), 3, 4, 5, 6),
-]
-
 # example
 # from hexpansion import hexpansion
-# rabbit = hexpansion[3]
+# rabbit = hexpansion(3)
 # red_eye = rabbit.ls["E"]
 # red_eye.init(rabbit.OUT)
 # red_eye.off()
@@ -37,4 +107,4 @@ hexpansion = [
 # red_eye.init(rabbit.PWM)
 # red_eye.pwm(100)
 # from machine import SPI
-# hex_spi = SPI(1, 40000000, sck=hexpansion[1].hs["A"], mosi=hexpansion[1].hs["B"])
+# hex_spi = SPI(1, 40000000, sck=rabbit.hs["A"], mosi=rabbit.hs["B"])
