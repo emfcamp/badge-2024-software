fdc5da3e671e2de9ff539bf81aa1e2d476c1d9e3
Matthew Wilkes <git@matthewwilkes.name>
2024-06-16 18:14:44 +0100

    Change off pattern fps to 1
    The old code set fps to 0, meaning the pattern is never checked again. Changing this to 1 means the background task won't actually exit when set to the off pattern, which allows changing the pattern without a reboot

--- modules/patterns/off.py
+++ modules/patterns/off.py
@@ -4,5 +4,5 @@ from patterns.base import BasePattern
 class OffPattern(BasePattern):
     def __init__(self):
         super().__init__()
-        self.fps = 0
+        self.fps = 1
         self.frames = [[(0, 0, 0)] * 12]
