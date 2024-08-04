17e062999b214938d737b35cf23b1ec3ec5dbcb0
Chris <ChrisDick@users.noreply.github.com>
2024-08-04 19:51:55 +0100

    add power fake for sim

---
+++ sim/fakes/power.py
@@ -0,0 +1,31 @@
+
+
+def Off():
+    return
+    
+def BatteryChargeState(): 
+    return "Not Charging"
+
+def BatteryLevel():
+    return 95.0
+
+def Enable5V():
+    return
+
+def Fault():
+    return {'Charge': 'Normal', 'Battery': 'Normal', 'Boost': 'Normal'}
+
+def SupplyCapabilities():
+    return [ ('fixed', 3000, 5)]
+
+def Icharge():
+    return 0.0
+
+def Vbat():
+    return 3.7
+
+def Vin():
+    return 5.0
+    
+def Vsys():
+    return 4.2    
