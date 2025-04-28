7f792f4ef41423e0c009ee0b4dc22cd0e542e827
Molive <moliveofscratch@gmail.com>
2025-04-28 23:07:49 +0100

    Remove tildagon_usb

--- drivers/micropython.cmake
+++ drivers/micropython.cmake
@@ -7,9 +7,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/tildagon_helpers/micropython.cmake)
 # Add the display driver
 include(${CMAKE_CURRENT_LIST_DIR}/gc9a01/micropython.cmake)
 
-# Add the HID interface
-include(${CMAKE_CURRENT_LIST_DIR}/tildagon_usb/tildagon_usb.cmake)
-
 # Add TCA9548A I2C MUX and micropython machine.I2C bindings
 include(${CMAKE_CURRENT_LIST_DIR}/tildagon_i2c/tildagon_i2c.cmake)
 
