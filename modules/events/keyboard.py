8882711903beaa693efb7ce1d4e508368a96783f
Matthew Wilkes <git@matthewwilkes.name>
2025-08-26 21:18:13 +0100

    Add events for common keyboard buttons
    I expect that there will be a number of Hexpansions that handle some form of text input. This adds common keyboard buttons to the list of inputs known by the firmware, and updates the text entry dialogue to accept them.

---
+++ modules/events/keyboard.py
@@ -0,0 +1,30 @@
+
+from .input import Button, BUTTON_TYPES
+
+letters = {
+    letter: Button(letter, "Keyboard") for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+}
+
+numbers = {
+    number: Button(number, "Keyboard") for number in '0123456789'
+}
+
+symbols = {
+    symbol: Button(symbol, "Keyboard") for symbol in '''!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'''
+}
+
+modifiers = {
+    "SPACE": Button("SPACE", "Keyboard"),
+    "SHIFT": Button("SHIFT", "Keyboard"),
+    "CTRL": Button("CTRL", "Keyboard"),
+    "ALT": Button("ALT", "Keyboard"),
+    "ESCAPE": Button("ESCAPE", "Keyboard", BUTTON_TYPES["CANCEL"]),
+    "DELETE": Button("DELETE", "Keyboard"),
+    "ENTER": Button("ENTER", "Keyboard", BUTTON_TYPES["CONFIRM"]),
+    "UP": Button("UP", "Keyboard", BUTTON_TYPES["UP"]),
+    "DOWN": Button("DOWN", "Keyboard", BUTTON_TYPES["DOWN"]),
+    "LEFT": Button("LEFT", "Keyboard", BUTTON_TYPES["LEFT"]),
+    "RIGHT": Button("RIGHT", "Keyboard", BUTTON_TYPES["RIGHT"]),
+}
+
+KEYBOARD_BUTTONS = letters | numbers | symbols | modifiers
