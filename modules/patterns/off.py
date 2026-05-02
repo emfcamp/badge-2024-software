8c686c2ac7620bc793b4b3911d9e2599b1a596df
Matthew Wilkes <git@matthewwilkes.name>
2026-05-03 00:31:13 +0100

    Make patterns independent of number of LEDs
    Introduce an argument for patterns (and fallback code for if it's not accepted) to allow specifying a different number of LEDs in the pattern to the default 12. This allows people to make use of the patterns in other places in future. Currently hard-coded to 12 LEDs in the pattern display.

--- modules/patterns/off.py
+++ modules/patterns/off.py
@@ -2,7 +2,7 @@ from patterns.base import BasePattern
 
 
 class OffPattern(BasePattern):
-    def __init__(self):
+    def __init__(self, num_leds=12):
         super().__init__()
         self.fps = 1
-        self.frames = [[(0, 0, 0)] * 12]
+        self.frames = [[(0, 0, 0)] * self.num_leds]
