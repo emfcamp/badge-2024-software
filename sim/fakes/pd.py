6fa0b26a39c9af3172a6ffa0de9a2af133552155
Chris <ChrisDick@users.noreply.github.com>
2026-05-10 19:50:29 +0100

    add pd Vendor messages and badge detection (#248)
    * add pd Vendor messages and badge detection
    
    * remove unused functions causing CI build failure
    
    * improve mp api
    
    * simplify send and get api
    
    * fix prime and double prime messages
    
    * simplify events
    
    * add fakes for sim
    
    * move badge detect to python
    
    * remove duplicate define
    
    * remove config based define
    
    ---------
    
    Co-authored-by: Matthew Wilkes <git@matthewwilkes.name>

---
+++ sim/fakes/pd.py
@@ -0,0 +1,14 @@
+class Host:
+    def send_prime_msg(self, data):
+        pass
+    
+    def send_dbl_prime_msg(self, data):
+        pass
+    
+    def send_vendor_msg(self, data):
+        pass
+    
+    
+class Device:
+    def send_vendor_msg(self, data):
+        pass
--- sim/fakes/power_event.py
+++ sim/fakes/power_event.py
@@ -21,3 +21,28 @@ def set_lanyard_attach_cb(handler):
 
 def set_lanyard_detach_cb(handler):
     pass
+
+def set_badge_as_device_attach_cb(handler):
+    pass
+
+def set_badge_as_device_detach_cb(handler):
+    pass
+
+def set_badge_as_host_attach_cb(handler):
+    pass
+
+def set_badge_as_host_detach_cb(handler):
+    pass
+
+def set_device_vendor_msg_rx_cb(handler):
+    pass
+
+def set_host_vendor_msg_rx_cb(handler):
+    pass
+
+def set_host_prime_msg_rx_cb(handler):
+    pass
+
+def set_host_dbl_prime_msg_rx_cb(handler):
+    pass
+
