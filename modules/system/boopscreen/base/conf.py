bea5e42d891ba84f1d5a9a8d0a2fb147bf8239b7
pikesley <sam.pikesley@demandlogic.co.uk>
2026-05-23 23:46:38 +0100

    Spin faster
    It runs slower on the Actual Badge than the simulator

--- modules/system/boopscreen/base/conf.py
+++ modules/system/boopscreen/base/conf.py
@@ -3,7 +3,7 @@ conf = {
     "bars": {"height": 1.25, "offset": 0.893, "width": 0.223},
     "circle": {"radius": 1.0, "ring-width": 0.179},
     "fade-rate": 0.1,
-    "growth-rate": 3,
+    "growth-rate": 10,
     "hue-increment": 0.03,
     "leds": {"increment": 0.01, "max": 0.5},
     "max-scale": 120,
