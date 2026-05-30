f537ad05c4bdd9173763a15e86a3b85ea9830f49
Chris <ChrisDick@users.noreply.github.com>
2026-05-30 09:54:22 +0100

    add api for compass

--- drivers/tildagon_imu/mp_imu.c
+++ drivers/tildagon_imu/mp_imu.c
@@ -32,6 +32,19 @@ static mp_obj_t mp_imu_gyro_read(void) {
 
 static MP_DEFINE_CONST_FUN_OBJ_0(mp_imu_gyro_read_obj, mp_imu_gyro_read);
 
+static mp_obj_t mp_imu_mag_read(void) {
+    static float x, y, z;
+
+    // Will not overwrite old data if there is an error
+    tildagon_imu_compass_read(&x, &y, &z);
+
+    mp_obj_t items[3] = { mp_obj_new_float(x), mp_obj_new_float(y),
+                          mp_obj_new_float(z) };
+    return mp_obj_new_tuple(3, items);
+}
+
+static MP_DEFINE_CONST_FUN_OBJ_0(mp_imu_mag_read_obj, mp_imu_mag_read);
+
 static mp_obj_t mp_imu_step_counter_read(void) {
     static uint32_t steps;
 
@@ -98,6 +111,7 @@ static MP_DEFINE_CONST_FUN_OBJ_2(mp_imu_write_to_obj, mp_imu_write_to);
 static const mp_rom_map_elem_t globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_acc_read), MP_ROM_PTR(&mp_imu_acc_read_obj) },
     { MP_ROM_QSTR(MP_QSTR_gyro_read), MP_ROM_PTR(&mp_imu_gyro_read_obj) },
+    { MP_ROM_QSTR(MP_QSTR_mag_read), MP_ROM_PTR(&mp_imu_mag_read_obj) },
     { MP_ROM_QSTR(MP_QSTR_step_counter_read), MP_ROM_PTR(&mp_imu_step_counter_read_obj) },
     { MP_ROM_QSTR(MP_QSTR_temperature_read), MP_ROM_PTR(&mp_imu_temperature_read_obj) },
     { MP_ROM_QSTR(MP_QSTR_id), MP_ROM_PTR(&mp_imu_id_obj) },
