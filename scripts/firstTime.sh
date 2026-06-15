73a3ef75f55f03b1bf8963a2bf3b775f17c79290
Sam Machin <sam@sammachin.com>
2026-06-15 09:08:41 +0100

    move patch to micopython.diff

--- scripts/firstTime.sh
+++ scripts/firstTime.sh
@@ -4,7 +4,6 @@ set -e
 pushd micropython
 git reset --hard
 git apply ../patches/micropython.diff
-git apply ../patches/nimble-our-sec-by-index.diff
 popd
 pushd micropython/lib/micropython-lib
 git reset --hard
