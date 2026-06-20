3ca5266f675d422ba686b7cd2db01e0e9a023112
Sam Machin <sam@sammachin.com>
2026-06-20 09:11:30 +0100

    remove reset from ism6ds3

--- drivers/tildagon_imu/lsm6ds3/lsm6ds3.h
+++ drivers/tildagon_imu/lsm6ds3/lsm6ds3.h
@@ -8,7 +8,6 @@ extern void lsm6ds3_task(void *data);
 extern void lsm6ds3_read_acc_mps(float *x, float *y, float *z);
 extern void lsm6ds3_read_gyro_dps(float *x, float *y, float *z);
 extern void lsm6ds3_read_steps(uint32_t *steps);
-extern void lsm6ds3_reset_steps(void);
 extern void lsm6ds3_read_temperature(float *temperature);
 extern int  lsm6ds3_write(uint8_t reg_addr, uint8_t *reg_data, uint8_t len );
 extern int  lsm6ds3_read(uint8_t reg_addr, uint8_t *reg_data, uint8_t len );
