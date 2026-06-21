2f658dee95a5e58cbc3774d2931e96550b909cd5
Matthew Wilkes <git@matthewwilkes.name>
2026-06-21 18:54:59 +0100

    Expose CTX font importer to micropython
    This exposes the version of the ctx font import machinery that takes raw ctx draw commands to micropython.

--- drivers/gc9a01/mp_uctx.h
+++ drivers/gc9a01/mp_uctx.h
@@ -19,3 +19,4 @@ typedef struct _mp_ctx_obj_t {
 extern const mp_obj_type_t mp_ctx_type;
 
 mp_obj_t mp_ctx_from_ctx(Ctx *ctx);
+int ctx_load_font_ctx (const char *name, const void *data, int length);
