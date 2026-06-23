9886c84843b44dfe345f4de001f23278617db2cf
Matthew Wilkes <git@matthewwilkes.name>
2026-06-23 13:11:14 +0100

    Placeholder fake for 2026 c support code

---
+++ sim/fakes/frontboard2026.py
@@ -0,0 +1,28 @@
+TOUCH1 = None
+TOUCH2 = None
+TOUCH3 = None
+TOUCH4 = None
+TOUCH5 = None
+TOUCH6 = None
+TOUCH7 = None
+TOUCH8 = None
+TOUCH9 = None
+TOUCH10 = None
+TOUCH11 = None
+TOUCH12 = None
+
+PROX1 = None
+PROX2 = None
+
+IRQ_RISING = 0
+IRQ_FALLING = 1
+
+
+def init(fb):
+    return
+
+def set_cb(button, handler, edge):
+    return
+
+def run():
+    return
