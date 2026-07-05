760859281a01c335f64cfb4a55f7bcb996a4876a
Andrew Shirley <andrew@als-bi.com>
2026-07-05 23:12:58 +0100

    Fix ruff failures

--- modules/patterns/emf2026.py
+++ modules/patterns/emf2026.py
@@ -1,8 +1,9 @@
 from patterns.palette import PalettePattern
 
+
 class Emf2026Pattern(PalettePattern):
     def __init__(self, num_leds=12):
-        #super().__init__(num_leds, [
+        # super().__init__(num_leds, [
         #    [0, 245, 81, 94], # coral
         #    [42, 247, 127, 2], # orange
         #    [84, 249, 226, 0], # yellow
@@ -10,9 +11,14 @@ class Emf2026Pattern(PalettePattern):
         #    [168, 46, 173, 217], # light blue
         #    [210, 0, 93, 150], # mid blue
         #    # [218, 0, 7, 48], # dark blue #This is just "off" for the LEDs so don't bother with it.
-        #], 120, 3)
-        super().__init__(num_leds, [
-            [0, 245, 81, 94], # coral
-            [84, 247, 127, 2], # orange
-            [168, 249, 226, 0], # yellow
-        ], 60, 3)
+        # ], 120, 3)
+        super().__init__(
+            num_leds,
+            [
+                [0, 245, 81, 94],  # coral
+                [84, 247, 127, 2],  # orange
+                [168, 249, 226, 0],  # yellow
+            ],
+            60,
+            3,
+        )
