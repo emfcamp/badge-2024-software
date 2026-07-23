00b3049e4cf909e45e7e45ba216fbc855facb14b
grahamparks <graham@utsire.com>
2026-07-23 17:34:20 +0200

    Fixed color references in twnetysix

--- modules/frontboards/twentysix.py
+++ modules/frontboards/twentysix.py
@@ -225,8 +225,8 @@ class TwentyTwentySix(FrontBoard):
             "button_text": self.colors["black"],
             "active_button_background": self.colors["yellow"],
             "active_button_text": self.colors["black"],
-            "definition_label": colors["orange"],
-            "active_definition_label": colors["yellow"],
+            "definition_label": self.colors["orange"],
+            "active_definition_label": self.colors["yellow"],
             "notification": self.colors["pink"],
             "notification_text": self.colors["black"],
         }
